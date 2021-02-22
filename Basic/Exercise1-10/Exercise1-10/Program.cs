e1ef446d2ded96c2a17930bc54e1a5c018573e6f
Shah Neoaj Kabir <shah35-1928@diu.edu.bd>
2021-02-22 23:17:41 +0600

    First Commit

---
+++ Basic/Exercise1-10/Exercise1-10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1
+            string First = "Hello";
+            string Second = "Shah Neoaj";
+            Console.WriteLine("{0} : {1}", First, Second);
+            Console.ReadKey();
+        }
+    }
+}
